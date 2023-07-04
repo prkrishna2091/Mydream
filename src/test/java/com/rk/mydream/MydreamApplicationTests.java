42d9972f0bbc60295dcf349ec4af8cfd1383fff5
prkrishna2091 <prkrishna2091@gmail.com>
2023-07-04 10:40:19 +0530

    mydream first commoit

---
+++ src/test/java/com/rk/mydream/MydreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.mydream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
